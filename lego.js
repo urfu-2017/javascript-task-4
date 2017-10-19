c61b6c519f42089c4840322cfeb622c71deb67fb
Ilya Kulygin <ruserixsian@gmail.com>
2017-10-19 23:54:16 +0500

    Done

--- lego.js
+++ lego.js
@@ -1,5 +1,15 @@
 'use strict';
 
+const SELECTORS_ORDER = {
+    filterIn: 1,
+    and: 2,
+    or: 3,
+    sortBy: 4,
+    select: 5,
+    limit: 6,
+    format: 7
+};
+
 /**
  * Сделано задание на звездочку
  * Реализованы методы or и and
@@ -12,60 +22,81 @@ exports.isStar = true;
  * @params {...Function} – Функции для запроса
  * @returns {Array}
  */
-exports.query = function (collection) {
-    return collection;
-};
+exports.query = (collection, ...selectors) =>
+    selectors
+        .sort((s1, s2) => SELECTORS_ORDER[s1.name] - SELECTORS_ORDER[s2.name])
+        .reduce((result, selector) => selector(result), collection);
 
 /**
  * Выбор полей
  * @params {...String}
+ * @returns {Function}
  */
-exports.select = function () {
-    return;
-};
+exports.select = (...properties) =>
+    function select(collection) {
+        return collection.map(item =>
+            properties.reduce((result, property) => {
+                if (item[property] !== undefined) {
+                    result[property] = item[property];
+                }
+
+                return result;
+            }, {})
+        );
+    };
 
 /**
  * Фильтрация поля по массиву значений
  * @param {String} property – Свойство для фильтрации
  * @param {Array} values – Доступные значения
+ * @returns {Function}
  */
-exports.filterIn = function (property, values) {
-    console.info(property, values);
-
-    return;
-};
+exports.filterIn = (property, values) =>
+    function filterIn(collection) {
+        return collection.filter(item => values.some(value => item[property] === value));
+    };
 
 /**
  * Сортировка коллекции по полю
  * @param {String} property – Свойство для фильтрации
  * @param {String} order – Порядок сортировки (asc - по возрастанию; desc – по убыванию)
+ * @returns {Function}
  */
-exports.sortBy = function (property, order) {
-    console.info(property, order);
+exports.sortBy = (property, order) =>
+    function sortBy(collection) {
+        order = order === 'asc' ? 1 : -1;
 
-    return;
-};
+        return collection.slice().sort((item1, item2) => {
+            if (item1[property] === item2[property]) {
+                return 0;
+            }
+
+            return item1[property] > item2[property] ? order : -order;
+        });
+    };
 
 /**
  * Форматирование поля
  * @param {String} property – Свойство для фильтрации
  * @param {Function} formatter – Функция для форматирования
+ * @returns {Function}
  */
-exports.format = function (property, formatter) {
-    console.info(property, formatter);
-
-    return;
-};
+exports.format = (property, formatter) =>
+    function format(collection) {
+        return collection.map(item =>
+            Object.assign({}, item, { [property]: formatter(item[property]) })
+        );
+    };
 
 /**
  * Ограничение количества элементов в коллекции
  * @param {Number} count – Максимальное количество элементов
+ * @returns {Function}
  */
-exports.limit = function (count) {
-    console.info(count);
-
-    return;
-};
+exports.limit = count =>
+    function limit(collection) {
+        return collection.slice(0, count);
+    };
 
 if (exports.isStar) {
 
@@ -73,17 +104,25 @@ if (exports.isStar) {
      * Фильтрация, объединяющая фильтрующие функции
      * @star
      * @params {...Function} – Фильтрующие функции
+     * @returns {Function}
      */
-    exports.or = function () {
-        return;
-    };
+    exports.or = (...selectors) =>
+        function or(collection) {
+            return collection.filter(item =>
+                selectors.some(selector => selector([item]).length > 0)
+            );
+        };
 
     /**
      * Фильтрация, пересекающая фильтрующие функции
      * @star
      * @params {...Function} – Фильтрующие функции
+     * @returns {Function}
      */
-    exports.and = function () {
-        return;
-    };
+    exports.and = (...selectors) =>
+        function or(collection) {
+            return collection.filter(item =>
+                selectors.every(selector => selector([item]).length > 0)
+            );
+        };
 }
