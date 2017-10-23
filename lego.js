5c97cb0183470b03d13f9d764b9ba985aef4f24b
Di <mm.di@mail.ru>
2017-10-24 04:52:18 +0500

    fixes5

--- lego.js
+++ lego.js
@@ -27,7 +27,7 @@ exports.isStar = true;
 exports.query = (collection, ...filters) => filters
     .sort((firstFunc, secondFunc) =>
         getOperationPriority(secondFunc) - getOperationPriority(firstFunc))
-    .reduce((summary, delegate) => delegate(summary), collection);
+    .reduce((summary, delegate) => delegate(summary), collection.map(x => x));
 
 /**
  * Выбор полей
