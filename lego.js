f4f81ac5b4e9906222b9367a175a6711ee443db2
Behemothhh <elakov111@gmail.com>
2017-10-23 22:19:33 +0500

    first version

--- lego.js
+++ lego.js
@@ -6,65 +6,111 @@
  */
 exports.isStar = true;
 
+const FUNCTION_ORDER = {
+    filterIn: 1,
+    or: 2,
+    and: 3,
+    sortBy: 4,
+    select: 5,
+    limit: 6,
+    format: 7
+};
+
+function copyCollection(collection) {
+    return collection.slice();
+}
+
 /**
  * Запрос к коллекции
  * @param {Array} collection
  * @params {...Function} – Функции для запроса
  * @returns {Array}
  */
-exports.query = function (collection) {
-    return collection;
+exports.query = function (collection, ...params) {
+    let collectionCopy = copyCollection(collection);
+    params.sort((a, b) => FUNCTION_ORDER[a.name] - FUNCTION_ORDER[b.name])
+        .forEach(func => {
+            collectionCopy = func(collectionCopy);
+            console.info(collectionCopy);
+        });
+
+    return collectionCopy;
 };
 
 /**
  * Выбор полей
  * @params {...String}
+ * @returns {Function}
  */
-exports.select = function () {
-    return;
+exports.select = (...params) => {
+    return function select(collection) {
+        return collection.map(person => {
+            let tempPerson = {};
+            params.forEach(prop => {
+                if (prop in person) {
+                    tempPerson[prop] = person[prop];
+                }
+            });
+
+            return tempPerson;
+        });
+    };
 };
 
 /**
  * Фильтрация поля по массиву значений
  * @param {String} property – Свойство для фильтрации
  * @param {Array} values – Доступные значения
+ * @returns {Function}
  */
-exports.filterIn = function (property, values) {
-    console.info(property, values);
+exports.filterIn = (property, values) => {
+    return function filterIn(collection) {
+        let tempCollection = [];
+        tempCollection = collection.filter(person => {
+            return values.some(item => person[property] === item);
+        });
 
-    return;
+        return tempCollection;
+    };
 };
 
 /**
  * Сортировка коллекции по полю
  * @param {String} property – Свойство для фильтрации
  * @param {String} order – Порядок сортировки (asc - по возрастанию; desc – по убыванию)
+ * @returns {Function}
  */
-exports.sortBy = function (property, order) {
-    console.info(property, order);
-
-    return;
+exports.sortBy = (property, order) => {
+    return function sortBy(collection) {
+        return collection.sort((a, b) => (a[property] - b[property]) * (order === 'asc' ? 1 : -1));
+    };
 };
 
 /**
  * Форматирование поля
  * @param {String} property – Свойство для фильтрации
  * @param {Function} formatter – Функция для форматирования
+ * @returns {Function}
  */
-exports.format = function (property, formatter) {
-    console.info(property, formatter);
+exports.format = (property, formatter) => {
+    return function format(collection) {
+        return collection.map(person => {
+            person[property] = formatter(person[property]);
 
-    return;
+            return person;
+        });
+    };
 };
 
 /**
  * Ограничение количества элементов в коллекции
  * @param {Number} count – Максимальное количество элементов
+ * @returns {Function}
  */
-exports.limit = function (count) {
-    console.info(count);
-
-    return;
+exports.limit = (count) => {
+    return function limit(collection) {
+        return collection.slice(0, count);
+    };
 };
 
 if (exports.isStar) {
@@ -73,17 +119,27 @@ if (exports.isStar) {
      * Фильтрация, объединяющая фильтрующие функции
      * @star
      * @params {...Function} – Фильтрующие функции
+     * @returns {Function}
      */
-    exports.or = function () {
-        return;
+    exports.or = (...commands) => {
+        return function or(collection) {
+            return collection.filter(person => {
+                return commands.some(rule => rule(collection).includes(person));
+            });
+        };
     };
 
     /**
      * Фильтрация, пересекающая фильтрующие функции
      * @star
      * @params {...Function} – Фильтрующие функции
+     * @returns {Function}
      */
-    exports.and = function () {
-        return;
+    exports.and = (...commands) => {
+        return function and(collection) {
+            return collection.filter(person => {
+                return commands.every(rule => rule(collection).includes(person));
+            });
+        };
     };
 }
