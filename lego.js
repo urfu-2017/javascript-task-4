576da65da2cd2bb1c2df983e54630736f6b0c46d
Ftlka <smart.Julia@yandex.ru>
2017-10-23 08:10:53 +0500

    Check if passes tests after changing to object

--- lego.js
+++ lego.js
@@ -3,19 +3,18 @@
 exports.isStar = true;
 
 const OPERATION_PRIORITY = ['filterIn', 'and', 'or', 'sortBy', 'select', 'limit', 'format'];
-const priority = name => OPERATION_PRIORITY.indexOf(name);
 
 const copyCollection = collection => JSON.parse(JSON.stringify(collection));
-const getFuncName = func => func.toString().split(/[^A-Za-z]/)[4];
 
-exports.query = (collection, ...params) =>
-    params
-        .sort((a, b) => priority(getFuncName(a)) - priority(getFuncName(b)))
-        .reduce((collectionCopy, param) => param(collectionCopy), copyCollection(collection));
+exports.query = (collection, ...params) => {
+    return params
+        .sort((a, b) => a.priority - b.priority)
+        .reduce((collectionCopy, param) => param.func(collectionCopy), copyCollection(collection));
+};
 
-function select(collection, params) {
-    return collection.reduce((result, entry) => {
-        let newEntry = params.reduce((element, param) => {
+const select = (collection, params) =>
+    collection.reduce((result, entry) => {
+        const newEntry = params.reduce((element, param) => {
             if (entry[param] !== undefined) {
                 element[param] = entry[param];
             }
@@ -26,17 +25,22 @@ function select(collection, params) {
 
         return result;
     }, []);
-}
 
-exports.select = (...params) => collection => select(collection, params);
+exports.select = (...params) => ({
+    priority: OPERATION_PRIORITY.indexOf('select'),
+    func: collection => select(collection, params)
+});
 
 const filterIn = (collection, property, values) => collection
     .filter(entry => values.includes(entry[property]));
 
-exports.filterIn = (property, values) => collection => filterIn(collection, property, values);
+exports.filterIn = (property, values) => ({
+    priority: OPERATION_PRIORITY.indexOf('filterIn'),
+    func: collection => filterIn(collection, property, values)
+});
 
 const sortBy = (collection, property, order) => {
-    let copy = copyCollection(collection);
+    const copy = copyCollection(collection);
     if (order === 'asc') {
         return copy.sort((a, b) => a[property] > b[property]);
     }
@@ -44,7 +48,10 @@ const sortBy = (collection, property, order) => {
     return copy.sort((a, b) => a[property] < b[property]);
 };
 
-exports.sortBy = (property, order) => collection => sortBy(collection, property, order);
+exports.sortBy = (property, order) => ({
+    priority: OPERATION_PRIORITY.indexOf('sortBy'),
+    func: collection => sortBy(collection, property, order)
+});
 
 const format = (collection, property, formatter) =>
     collection.map(entry => {
@@ -56,24 +63,36 @@ const format = (collection, property, formatter) =>
     });
 
 
-exports.format = (property, formatter) => collection => format(collection, property, formatter);
+exports.format = (property, formatter) => ({
+    priority: OPERATION_PRIORITY.indexOf('format'),
+    func: collection => format(collection, property, formatter)
+});
 
 const limit = (collection, count) => collection.slice(0, count);
 
-exports.limit = count => collection => limit(collection, count);
+exports.limit = count => ({
+    priority: OPERATION_PRIORITY.indexOf('limit'),
+    func: collection => limit(collection, count)
+});
 
 if (exports.isStar) {
     const or = (collection, params) =>
         collection.filter(entry =>
-            params.some(param => param([entry]).length)
+            params.some(param => param.func([entry]).length)
         );
 
-    exports.or = (...params) => collection => or(collection, params);
+    exports.or = (...params) => ({
+        priority: OPERATION_PRIORITY.indexOf('or'),
+        func: collection => or(collection, params)
+    });
 
     const and = (collection, params) =>
         collection.filter(entry =>
-            params.every(param => param([entry]).length)
+            params.every(param => param.func([entry]).length)
         );
 
-    exports.and = (...params) => collection => and(collection, params);
+    exports.and = (...params) => ({
+        priority: OPERATION_PRIORITY.indexOf('and'),
+        func: collection => and(collection, params)
+    });
 }
