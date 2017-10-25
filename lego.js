636bf855f9ad82543aac4cb7e64f11efd43e3048
reindeermur <sofiabochik@gmail.com>
2017-10-26 01:35:33 +0500

    Update lego.js

--- lego.js
+++ lego.js
@@ -7,14 +7,14 @@
 exports.isStar = true;
 
 const PRIORITY_LIST = {
-    select : 1,
-    filterIn : 0,
-    sortBy : 1,
-    format : 2,
-    limit : 2,
-    or : 0,
-    and : 0
-} 
+    select: 1,
+    filterIn: 0,
+    sortBy: 1,
+    format: 2,
+    limit: 2,
+    or: 0,
+    and: 0
+}; 
 
 /**
  * Запрос к коллекции
@@ -24,43 +24,44 @@ const PRIORITY_LIST = {
  */
 exports.query = function (collection, ...functions) {
     let copyCollection = getCopy(collection);
-    functions = functions.sort((a, b) => PRIORITY_LIST[a.name]-PRIORITY_LIST[b.name]);
+    functions = functions.sort((a, b) => PRIORITY_LIST[a.name] - PRIORITY_LIST[b.name]);
     for (const element of functions) {
         copyCollection = element(copyCollection);
     }
 
-    return copyCollection
+    return copyCollection;
 };
 
 
 function getCopy(collection) {
-    return JSON.parse(JSON.stringify(object));
+    return JSON.parse(JSON.stringify(collection));
 }
 
 /**
  * Выбор полей
  * @params {...String}
+ * @returns {Function}
  */
 exports.select = function (...fields) {
     return function select(collection) {
         return collection.map(element => {
             let newElement = {};
             for (const field of Object.keys(element)) {
-                if (fiels.includes(field)){
+                if (fields.includes(field)) {
                     newElement[field] = element[field];
                 }
             }
 
-            return newElement
+            return newElement;
         });
     };
-    
 };
 
 /**
  * Фильтрация поля по массиву значений
  * @param {String} property – Свойство для фильтрации
  * @param {Array} values – Доступные значения
+ * @returns {Function}
  */
 exports.filterIn = function (property, values) {
     return function filterIn(collection){
@@ -71,10 +72,11 @@ exports.filterIn = function (property, values) {
  * Сортировка коллекции по полю
  * @param {String} property – Свойство для фильтрации
  * @param {String} order – Порядок сортировки (asc - по возрастанию; desc – по убыванию)
+ * @returns {Function}
  */
 exports.sortBy = function (property, order) {
     return function sortBy(collection) {
-        copyCollection = getCopy(collection);
+        let copyCollection = getCopy(collection);
         return copyCollection.sort((a, b) => {
             if (a[property] > b[property]) {
                 return 1;
@@ -82,7 +84,7 @@ exports.sortBy = function (property, order) {
             if (a[property] < b[property]) {
                 return -1;
             }
-            
+
             return 0;
         });
         if (order === 'asc') {
@@ -97,6 +99,7 @@ exports.sortBy = function (property, order) {
  * Форматирование поля
  * @param {String} property – Свойство для фильтрации
  * @param {Function} formatter – Функция для форматирования
+ * @returns {Function}
  */
 exports.format = function (property, formatter) {
     return function format(collection) {
@@ -109,6 +112,7 @@ exports.format = function (property, formatter) {
 /**
  * Ограничение количества элементов в коллекции
  * @param {Number} count – Максимальное количество элементов
+ * @returns {Functions}
  */
 exports.limit = function (count) {
     return function limit(collection) {
@@ -122,6 +126,7 @@ if (exports.isStar) {
      * Фильтрация, объединяющая фильтрующие функции
      * @star
      * @params {...Function} – Фильтрующие функции
+     * @returns {Function}
      */
     exports.or = function (...functions) {
         return function or(collection) {
@@ -133,10 +138,10 @@ if (exports.isStar) {
      * Фильтрация, пересекающая фильтрующие функции
      * @star
      * @params {...Function} – Фильтрующие функции
+     * @returns {Function}
      */
     exports.and = function (...functions) {
         return function and(collection) {
-            return function or(collection) {
                 return colection.filter(el => functions.every(func => func([el]).length > 0));
             };
         };
