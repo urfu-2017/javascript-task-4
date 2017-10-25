3cc0f485e639f4d527f33b20997c1185e5b64b09
reindeermur <sofiabochik@gmail.com>
2017-10-26 01:06:04 +0500

    1

--- lego.js
+++ lego.js
@@ -6,22 +6,55 @@
  */
 exports.isStar = true;
 
+const PRIORITY_LIST = {
+    select : 1,
+    filterIn : 0,
+    sortBy : 1,
+    format : 2,
+    limit : 2,
+    or : 0,
+    and : 0
+} 
+
 /**
  * Запрос к коллекции
  * @param {Array} collection
  * @params {...Function} – Функции для запроса
  * @returns {Array}
  */
-exports.query = function (collection) {
-    return collection;
+exports.query = function (collection, ...functions) {
+    let copyCollection = getCopy(collection);
+    functions = functions.sort((a, b) => PRIORITY_LIST[a.name]-PRIORITY_LIST[b.name]);
+    for (const element of functions) {
+        copyCollection = element(copyCollection);
+    }
+
+    return copyCollection
 };
+
+
+function getCopy(collection) {
+    return JSON.parse(JSON.stringify(object));
+}
 
 /**
  * Выбор полей
  * @params {...String}
  */
-exports.select = function () {
-    return;
+exports.select = function (...fields) {
+    return function select(collection) {
+        return collection.map(element => {
+            let newElement = {};
+            for (const field of Object.keys(element)) {
+                if (fiels.includes(field)){
+                    newElement[field] = element[field];
+                }
+            }
+
+            return newElement
+        });
+    };
+    
 };
 
 /**
@@ -30,9 +63,8 @@ exports.select = function () {
  * @param {Array} values – Доступные значения
  */
 exports.filterIn = function (property, values) {
-    console.info(property, values);
-
-    return;
+    return function filterIn(collection){
+        return collection.filterIn(element => values.includes(element[property]))};
 };
 
 /**
@@ -41,9 +73,24 @@ exports.filterIn = function (property, values) {
  * @param {String} order – Порядок сортировки (asc - по возрастанию; desc – по убыванию)
  */
 exports.sortBy = function (property, order) {
-    console.info(property, order);
+    return function sortBy(collection) {
+        copyCollection = getCopy(collection);
+        return copyCollection.sort((a, b) => {
+            if (a[property] > b[property]) {
+                return 1;
+            }
+            if (a[property] < b[property]) {
+                return -1;
+            }
+            
+            return 0;
+        });
+        if (order === 'asc') {
+            return copyCollection;
+        }
 
-    return;
+        return copyCollection.reverse();
+    };
 };
 
 /**
@@ -52,9 +99,11 @@ exports.sortBy = function (property, order) {
  * @param {Function} formatter – Функция для форматирования
  */
 exports.format = function (property, formatter) {
-    console.info(property, formatter);
-
-    return;
+    return function format(collection) {
+        collection.forEach((element) => {
+            element[property] = formatter(element[property]);
+        });
+    };
 };
 
 /**
@@ -62,9 +111,9 @@ exports.format = function (property, formatter) {
  * @param {Number} count – Максимальное количество элементов
  */
 exports.limit = function (count) {
-    console.info(count);
-
-    return;
+    return function limit(collection) {
+        return collection.slice(0, count);
+    };
 };
 
 if (exports.isStar) {
@@ -74,8 +123,10 @@ if (exports.isStar) {
      * @star
      * @params {...Function} – Фильтрующие функции
      */
-    exports.or = function () {
-        return;
+    exports.or = function (...functions) {
+        return function or(collection) {
+            return colection.filter(el => functions.some(func => func([el]).length > 0));
+        };
     };
 
     /**
@@ -83,7 +134,11 @@ if (exports.isStar) {
      * @star
      * @params {...Function} – Фильтрующие функции
      */
-    exports.and = function () {
-        return;
+    exports.and = function (...functions) {
+        return function and(collection) {
+            return function or(collection) {
+                return colection.filter(el => functions.every(func => func([el]).length > 0));
+            };
+        };
     };
 }
