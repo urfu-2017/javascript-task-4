838e7fb196c6d507800bd8dc2eeb2ee449e298dc
rkrk117 <32203360+rkrk117@users.noreply.github.com>
2017-10-25 20:02:21 +0500

    Add files via upload

--- lego.js
+++ lego.js
@@ -12,60 +12,157 @@ exports.isStar = true;
  * @params {...Function} – Функции для запроса
  * @returns {Array}
  */
-exports.query = function (collection) {
-    return collection;
+exports.query = function (collection, ...actions) {
+    var result = collection;
+    actions.forEach(function (action) {
+        if (action.type === 'filter') {
+            result = action.func(result);
+        }
+    });
+    actions.forEach(function (action) {
+        if (action.type === 'sort') {
+            result = action.func(result);
+        }
+    });
+    actions.forEach(function (action) {
+        if (action.type === 'limit') {
+            result = action.func(result);
+        }
+    });
+    actions.forEach(function (action) {
+        if (action.type === 'select') {
+            result = action.func(result);
+        }
+    });
+    actions.forEach(function (action) {
+        if (action.type === 'format') {
+            result = action.func(result);
+        }
+    });
+
+    return result;
 };
 
 /**
  * Выбор полей
  * @params {...String}
+ * @returns {Array}
  */
 exports.select = function () {
-    return;
+    var desiredValues = arguments;
+    var selectFunction = function (friends) {
+        return friends.map(function (friend) {
+            var result = {};
+            for (var property of desiredValues) {
+                if (typeof friend[property] !== 'undefined') {
+                    result[property] = friend[property];
+                }
+            }
+
+            return result;
+        });
+    };
+
+    return { type: 'select', func: selectFunction };
 };
 
 /**
  * Фильтрация поля по массиву значений
  * @param {String} property – Свойство для фильтрации
  * @param {Array} values – Доступные значения
+ * @returns {Array}
  */
 exports.filterIn = function (property, values) {
-    console.info(property, values);
+    var filterFunction = function (friends) {
+        return friends.filter(friend => values.indexOf(friend[property]) >= 0);
+    };
 
-    return;
+    return { type: 'filter', func: filterFunction };
 };
 
 /**
  * Сортировка коллекции по полю
  * @param {String} property – Свойство для фильтрации
  * @param {String} order – Порядок сортировки (asc - по возрастанию; desc – по убыванию)
+ * @returns {Array}
  */
 exports.sortBy = function (property, order) {
-    console.info(property, order);
+    var compareBy = function (a, b) {
+        if (a[property] > b[property]) {
+            return 1;
+        } else if (a[property] === b[property]) {
+            return 0;
+        }
 
-    return;
+        return -1;
+    };
+    var sortFunction;
+    if (order === 'asc') {
+        sortFunction = function (friends) {
+            friends.sort(compareBy);
+
+            return friends;
+        };
+    } else {
+        sortFunction = function (friends) {
+            friends.sort((a, b) => -compareBy(a, b));
+
+            return friends;
+        };
+    }
+
+    return { type: 'sort', func: sortFunction };
 };
 
 /**
  * Форматирование поля
  * @param {String} property – Свойство для фильтрации
  * @param {Function} formatter – Функция для форматирования
+ * @returns {Array}
  */
 exports.format = function (property, formatter) {
-    console.info(property, formatter);
+    var formatFunction = function (friends) {
+        return friends.map(function (friend) {
+            friend[property] = formatter(friend[property]);
 
-    return;
+            return friend;
+        });
+    };
+
+    return { type: 'format', func: formatFunction };
 };
 
 /**
  * Ограничение количества элементов в коллекции
  * @param {Number} count – Максимальное количество элементов
+ * @returns {Array}
  */
 exports.limit = function (count) {
-    console.info(count);
+    var limitFunction = function (friends) {
+        return friends.slice(0, count);
+    };
 
-    return;
+    return { type: 'limit', func: limitFunction };
 };
+
+function friendInParty(friend, party) {
+    function friendCompare(firstFriend, secondFriend) {
+        for (var property in firstFriend) {
+            if (secondFriend[property] !== firstFriend[property]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    for (var partyMember of party) {
+        if (friendCompare(friend, partyMember)) {
+            return true;
+        }
+    }
+
+    return false;
+}
 
 if (exports.isStar) {
 
@@ -73,17 +170,48 @@ if (exports.isStar) {
      * Фильтрация, объединяющая фильтрующие функции
      * @star
      * @params {...Function} – Фильтрующие функции
+     * @returns {Array}
      */
     exports.or = function () {
-        return;
+        function unitePartys(firstParty, secondParty) {
+            var result = firstParty.slice(0);
+            secondParty.forEach(function (member) {
+                if (!friendInParty(member, result)) {
+                    result.push(member);
+                }
+            });
+
+            return result;
+        }
+
+        var actions = Array.prototype.slice.call(arguments);
+        var orFunction = function (friends) {
+            var partysToUnite = actions.map((action) => action.func(friends));
+
+            return partysToUnite.reduce(unitePartys, []);
+        };
+
+        return { type: 'filter', func: orFunction };
     };
 
     /**
      * Фильтрация, пересекающая фильтрующие функции
      * @star
      * @params {...Function} – Фильтрующие функции
+     * @returns {Array}
      */
     exports.and = function () {
-        return;
+        function intersectPartys(firstParty, secondParty) {
+            return firstParty.filter(friend => friendInParty(friend, secondParty));
+        }
+
+        var actions = Array.prototype.slice.call(arguments);
+        var andFunction = function (friends) {
+            var partysToUnite = actions.map((action) => action.func(friends));
+
+            return partysToUnite.reduce(intersectPartys, partysToUnite[0]);
+        };
+
+        return { type: 'filter', func: andFunction };
     };
 }
