8f5d6104c860125d68bbc562354597c180e6b6c4
Gusarov Vadim <gusarovvadim1996@gmail.com>
2017-10-25 19:11:22 +0500

    Update lego.js

--- lego.js
+++ lego.js
@@ -4,7 +4,7 @@
  * Сделано задание на звездочку
  * Реализованы методы or и and
  */
-exports.isStar = true;
+exports.isStar = false;
 
 /**
  * Запрос к коллекции
@@ -13,58 +13,130 @@ exports.isStar = true;
  * @returns {Array}
  */
 exports.query = function (collection) {
-    return collection;
+    let functions = [].slice.call(arguments).slice(1);
+    functions = handlingOfFunctions(functions);
+    let resultCollection = collection.map(friend => Object.assign({}, friend));
+    let properties = [];
+    for (let _function of functions) {
+        if (_function.name !== '_select') {
+            resultCollection = _function(resultCollection);
+        } else {
+            let result = _function(resultCollection);
+            resultCollection = result[0];
+            properties = properties.concat(result[1]);
+        }
+    }
+    for (let friend of resultCollection) {
+        deleteUselessProperties(friend, properties);
+    }
+
+    return resultCollection;
 };
+
+function deleteUselessProperties(friend, properties) {
+    for (let property in friend) {
+        if (properties.indexOf(property) === -1) {
+            delete friend[property];
+        }
+    }
+}
 
 /**
  * Выбор полей
  * @params {...String}
+ * @returns {Function}
  */
 exports.select = function () {
-    return;
+    let argv = [].slice.call(arguments);
+
+    return function _select(collection) {
+        for (let friend of collection) {
+            let count = 0;
+            for (let property of Object.keys(friend)) {
+                count += argv.includes(property);
+            }
+            if (count === 0) {
+                collection.splice(collection.indexOf(friend), 1);
+            }
+        }
+
+        return [collection, argv];
+    };
 };
 
 /**
  * Фильтрация поля по массиву значений
  * @param {String} property – Свойство для фильтрации
  * @param {Array} values – Доступные значения
+ * @returns {Function}
  */
 exports.filterIn = function (property, values) {
-    console.info(property, values);
+    return function _filterIn(collection) {
+        for (let friend of collection) {
+            if ((values.indexOf(friend[property]) === -1)) {
+                collection.splice(collection.indexOf(friend), 1);
+            }
+        }
 
-    return;
+        return collection;
+    };
 };
 
 /**
  * Сортировка коллекции по полю
  * @param {String} property – Свойство для фильтрации
  * @param {String} order – Порядок сортировки (asc - по возрастанию; desc – по убыванию)
+ * @returns {Function}
  */
 exports.sortBy = function (property, order) {
-    console.info(property, order);
+    return function _sortBy(collection) {
+        collection.sort(function (a, b) {
+            let first = a[property];
+            let second = b[property];
 
-    return;
+            if (first < second) {
+                return -1;
+            } else if (first > second) {
+                return 1;
+            }
+
+            return 0;
+        });
+        if (order === 'desc') {
+            collection.reverse();
+        }
+
+        return collection;
+    };
 };
 
 /**
  * Форматирование поля
  * @param {String} property – Свойство для фильтрации
  * @param {Function} formatter – Функция для форматирования
+ * @returns {Function}
  */
 exports.format = function (property, formatter) {
-    console.info(property, formatter);
+    return function _format(collection) {
+        for (let friend of collection) {
+            friend[property] = formatter(friend[property]);
+        }
 
-    return;
+        return collection;
+    };
 };
 
 /**
  * Ограничение количества элементов в коллекции
  * @param {Number} count – Максимальное количество элементов
+ * @returns {Function}
  */
 exports.limit = function (count) {
-    console.info(count);
+    return function _limit(collection) {
+        collection = collection.slice(0, count);
 
-    return;
+        return collection;
+    };
 };
 
 if (exports.isStar) {
@@ -86,4 +158,19 @@ if (exports.isStar) {
     exports.and = function () {
         return;
     };
+}
+
+function handlingOfFunctions(functions) {
+    let prioritiesOfFunctions = {
+        '_filterIn': 1,
+        '_sortBy': 2,
+        '_select': 3,
+        '_format': 4,
+        '_limit': 5
+    };
+    functions.sort(function (a, b) {
+        return prioritiesOfFunctions[a.name] - prioritiesOfFunctions[b.name];
+    });
+
+    return functions;
 }
