52aaf1c818c35b0ff5eca851be02ff2ce9b4769b
Maxim <32168435+Rolyx2@users.noreply.github.com>
2017-10-25 16:45:48 +0500

    не работает

--- lego.js
+++ lego.js
@@ -1,10 +1,16 @@
 'use strict';
+const functionWeight = {
+    format: 1,
+    select: 2,
+    filterIn: 3,
+    sortBy: 4
+};
 
 /**
  * Сделано задание на звездочку
  * Реализованы методы or и and
  */
-exports.isStar = true;
+exports.isStar = false;
 
 /**
  * Запрос к коллекции
@@ -12,16 +18,45 @@ exports.isStar = true;
  * @params {...Function} – Функции для запроса
  * @returns {Array}
  */
-exports.query = function (collection) {
-    return collection;
+
+exports.query = function (collection, ...values) {
+    var clone = Object.assign([], collection);
+    const methods = Array.from(values)
+        .sort((firstFunction, secondFunction) => {
+            if (functionWeight[firstFunction.name] > functionWeight[secondFunction.name]) {
+                return -1;
+            }
+            if (functionWeight[firstFunction.name] < functionWeight[secondFunction.name]) {
+                return 1;
+            }
+
+            return 0;
+        });
+    methods.forEach(function (func) {
+        clone = func(clone);
+    });
+
+    return clone;
 };
 
 /**
  * Выбор полей
  * @params {...String}
  */
-exports.select = function () {
-    return;
+
+exports.select = function (...values) {
+    return function select(collection) {
+        return collection.map(function (data) {
+            Object.keys(data).forEach(function (key) {
+                if (!values.includes(key)) {
+                    delete data[key];
+                }
+            });
+
+            return data;
+        });
+    };
+
 };
 
 /**
@@ -29,10 +64,13 @@ exports.select = function () {
  * @param {String} property – Свойство для фильтрации
  * @param {Array} values – Доступные значения
  */
-exports.filterIn = function (property, values) {
-    console.info(property, values);
 
-    return;
+exports.filterIn = function (property, values) {
+    return function filterIn(collection) {
+        return collection.filter(function (data) {
+            return values.indexOf(data[property]) !== -1;
+        });
+    };
 };
 
 /**
@@ -40,10 +78,21 @@ exports.filterIn = function (property, values) {
  * @param {String} property – Свойство для фильтрации
  * @param {String} order – Порядок сортировки (asc - по возрастанию; desc – по убыванию)
  */
-exports.sortBy = function (property, order) {
-    console.info(property, order);
 
-    return;
+exports.sortBy = function (property, order) {
+    return function SortBy(collection) {
+        return collection.sort(function (firstValue, secondValue) {
+            if (order === 'asc') {
+                let typeAsc = Number(firstValue[property] > secondValue[property]);
+
+                return typeAsc;
+            }
+            let typeDesc = Number(firstValue[property] < secondValue[property]);
+
+            return typeDesc;
+        }
+        );
+    };
 };
 
 /**
@@ -51,10 +100,18 @@ exports.sortBy = function (property, order) {
  * @param {String} property – Свойство для фильтрации
  * @param {Function} formatter – Функция для форматирования
  */
-exports.format = function (property, formatter) {
-    console.info(property, formatter);
 
-    return;
+exports.format = function (property, formatter) {
+    return function format(collection) {
+        return collection.map(function (data) {
+            if (property in data) {
+                data[property] = formatter(data[property]);
+            }
+
+            return data;
+        }
+        );
+    };
 };
 
 /**
