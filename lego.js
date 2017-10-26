3ea76b9a3e4b30fc357a3614e2a4bc96b96c9fad
Chernyshev <sanocher@gmail.com>
2017-10-26 14:33:07 +0500

    solution

--- lego.js
+++ lego.js
@@ -4,7 +4,15 @@
  * Сделано задание на звездочку
  * Реализованы методы or и and
  */
-exports.isStar = true;
+exports.isStar = false;
+
+let FUNC_PRIORITIES = {
+    limit: 2,
+    format: 2,
+    select: 1,
+    sortBy: 0,
+    filterIn: 0
+};
 
 /**
  * Запрос к коллекции
@@ -12,59 +20,84 @@ exports.isStar = true;
  * @params {...Function} – Функции для запроса
  * @returns {Array}
  */
-exports.query = function (collection) {
-    return collection;
+exports.query = function (collection, ...functions) {
+    return functions.sort((a, b) => FUNC_PRIORITIES[a.name] - FUNC_PRIORITIES[b.name])
+        .reduce((value, func) => func(value), collection);
 };
 
 /**
  * Выбор полей
  * @params {...String}
+ * @returns {Function}
  */
-exports.select = function () {
-    return;
+exports.select = function (...fields) {
+    return function select(col) {
+        return col.map(e => {
+            let newElement = {};
+            for (var field of Object.keys(e)) {
+                if (fields.includes(field)) {
+                    newElement[field] = e[field];
+                }
+            }
+
+            return newElement;
+        });
+    };
 };
 
 /**
  * Фильтрация поля по массиву значений
  * @param {String} property – Свойство для фильтрации
  * @param {Array} values – Доступные значения
+ * @returns {Function}
  */
 exports.filterIn = function (property, values) {
-    console.info(property, values);
-
-    return;
+    return function filterIn(col) {
+        return col.filter(element => values.includes(element[property]));
+    };
 };
 
 /**
  * Сортировка коллекции по полю
  * @param {String} property – Свойство для фильтрации
  * @param {String} order – Порядок сортировки (asc - по возрастанию; desc – по убыванию)
+ * @returns {Function}
  */
 exports.sortBy = function (property, order) {
-    console.info(property, order);
+    return function sortBy(col) {
+        return col.sort(function (a, b) {
+            let ord = order === 'asc' ? 1 : -1;
 
-    return;
+            return ord * a[property] > b[property] ? 1 : -1;
+        });
+    };
 };
 
 /**
  * Форматирование поля
  * @param {String} property – Свойство для фильтрации
  * @param {Function} formatter – Функция для форматирования
+ * @returns {Function}
  */
 exports.format = function (property, formatter) {
-    console.info(property, formatter);
+    return function format(col) {
+        return col.map(obj => {
+            obj[property] = formatter(obj[property]);
 
-    return;
+            return obj;
+        });
+    };
 };
 
 /**
  * Ограничение количества элементов в коллекции
  * @param {Number} count – Максимальное количество элементов
+ * @returns {Function}
  */
 exports.limit = function (count) {
-    console.info(count);
-
-    return;
+    return function limit(col) {
+        return col.slice(0, count);
+    };
 };
 
 if (exports.isStar) {
