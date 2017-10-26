d7da1ba68ac6604f7ad7506b0329538f84a9a4ad
valerin5 <valera9719@gmail.com>
2017-10-26 17:49:36 +0500

    раз

--- lego.js
+++ lego.js
@@ -1,10 +1,20 @@
 'use strict';
 
+const PRIORITY = {
+    filterIn: 0,
+    and: 1,
+    or: 2,
+    sortBy: 3,
+    select: 5,
+    limit: 4,
+    format: 6
+};
+
 /**
  * Сделано задание на звездочку
  * Реализованы методы or и and
  */
-exports.isStar = true;
+exports.isStar = false;
 
 /**
  * Запрос к коллекции
@@ -12,16 +22,40 @@ exports.isStar = true;
  * @params {...Function} – Функции для запроса
  * @returns {Array}
  */
-exports.query = function (collection) {
-    return collection;
+
+
+exports.query = function (collection, ...selectors) {
+    var collectionCopy = collection;
+    selectors.sort((a, b) => PRIORITY[a.name] - PRIORITY[b.name]);
+    for (let i = 0; i < selectors.length; i++) {
+        collectionCopy = selectors[i](collectionCopy);
+    }
+
+    return collectionCopy;
 };
 
 /**
  * Выбор полей
  * @params {...String}
  */
-exports.select = function () {
-    return;
+
+exports.select = function (...params) {
+
+    return function (collection) {
+        let collectionCopy = collection;
+        collectionCopy = collectionCopy.map((person) => {
+            let newPerson = {};
+            for (var i = 0; i < params.length; i++) {
+                if (person[i] !== undefined) {
+                    newPerson[i] = person[i];
+                }
+            }
+
+            return newPerson;
+        });
+
+        return collectionCopy;
+    };
 };
 
 /**
@@ -29,10 +63,15 @@ exports.select = function () {
  * @param {String} property – Свойство для фильтрации
  * @param {Array} values – Доступные значения
  */
+
 exports.filterIn = function (property, values) {
     console.info(property, values);
 
-    return;
+    return function (collection) {
+        let collectionCopy = collection.filter((a) => values.indexOf(a[property]) > -1);
+
+        return collectionCopy;
+    };
 };
 
 /**
@@ -40,10 +79,21 @@ exports.filterIn = function (property, values) {
  * @param {String} property – Свойство для фильтрации
  * @param {String} order – Порядок сортировки (asc - по возрастанию; desc – по убыванию)
  */
-exports.sortBy = function (property, order) {
-    console.info(property, order);
 
-    return;
+exports.sortBy = function (property, order) {
+    // console.info(property, order);
+
+    return function (collection) {
+        let collectionCopy = collection;
+        if (order === 'asc') {
+            collectionCopy = collectionCopy.sort((a, b) => a[property] > b[property]);
+
+            return collectionCopy;
+        }
+        collectionCopy = collectionCopy.sort((a, b) => a[property] < b[property]);
+
+        return collectionCopy;
+    };
 };
 
 /**
@@ -51,20 +101,37 @@ exports.sortBy = function (property, order) {
  * @param {String} property – Свойство для фильтрации
  * @param {Function} formatter – Функция для форматирования
  */
-exports.format = function (property, formatter) {
-    console.info(property, formatter);
 
-    return;
+exports.format = function (property, formatter) {
+    // console.info(property, formatter);
+
+    return function (collection) {
+        let collectionCopy = collection;
+        collectionCopy = collectionCopy.map((person) => {
+            let newPerson = person;
+            newPerson[property] = formatter(person[property]);
+            console.info(newPerson);
+
+            return newPerson;
+        });
+
+        return collectionCopy;
+    };
 };
 
 /**
  * Ограничение количества элементов в коллекции
  * @param {Number} count – Максимальное количество элементов
  */
-exports.limit = function (count) {
-    console.info(count);
 
-    return;
+exports.limit = function (count) {
+    // console.info(count);
+
+    return function (collection) {
+        let collectionCopy = collection.slice(0, count);
+
+        return collectionCopy;
+    };
 };
 
 if (exports.isStar) {
