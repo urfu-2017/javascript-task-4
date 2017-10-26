14ca6ae5daf8cb5fc440c0d4207c77adc92b9370
ValeryZhurba <valera.zhurba@gmail.com>
2017-10-26 10:53:05 +0400

    done

--- lego.js
+++ lego.js
@@ -12,59 +12,108 @@ exports.isStar = true;
  * @params {...Function} – Функции для запроса
  * @returns {Array}
  */
+
+const FUNC_PRIORITIES = {
+    select: 1,
+    format: 2,
+    filterIn: 0,
+    sortBy: 0,
+    or: 0,
+    and: 0,
+    limit: 2
+};
+
+function copyCollection(collection) {
+    return collection.map((obj) => Object.assign({}, obj));
+}
+
 exports.query = function (collection) {
-    return collection;
+    let functions = [].slice.call(arguments, 1);
+    let collectionCopy = copyCollection(collection);
+
+    return functions.sort((first, second) =>
+        FUNC_PRIORITIES[first.name] - FUNC_PRIORITIES[second.name])
+        .reduce((acc, func) => func(acc), collectionCopy);
 };
 
 /**
  * Выбор полей
  * @params {...String}
+ * @returns {function}
  */
 exports.select = function () {
-    return;
+    const argumentsArr = [].slice.call(arguments);
+
+    return function select(collection) {
+        return collection.map(person => {
+            let resultPerson = {};
+
+            return argumentsArr.reduce(function (acc, key) {
+                acc[key] = person[key];
+
+                return acc;
+            }, resultPerson);
+        });
+    };
 };
 
 /**
  * Фильтрация поля по массиву значений
  * @param {String} property – Свойство для фильтрации
  * @param {Array} values – Доступные значения
+ * @returns {function}
  */
 exports.filterIn = function (property, values) {
-    console.info(property, values);
-
-    return;
+    return function filterIn(collection) {
+        return collection.filter(person => values.indexOf(person[property]) !== -1);
+    };
 };
 
 /**
  * Сортировка коллекции по полю
  * @param {String} property – Свойство для фильтрации
  * @param {String} order – Порядок сортировки (asc - по возрастанию; desc – по убыванию)
+ * @returns {function}
  */
 exports.sortBy = function (property, order) {
-    console.info(property, order);
+    return function sortBy(collection) {
+        const reverse = (order === 'asc') ? 1 : -1;
 
-    return;
+        return collection.sort((firstPerson, secondPerson) =>
+            reverse * (firstPerson[property] - secondPerson[property]));
+    };
 };
 
 /**
  * Форматирование поля
  * @param {String} property – Свойство для фильтрации
  * @param {Function} formatter – Функция для форматирования
+ * @returns {function}
  */
 exports.format = function (property, formatter) {
-    console.info(property, formatter);
+    return function format(collection) {
+        return collection.map(person => {
+            person[property] = formatter(person[property]);
 
-    return;
+            return person;
+        });
+    };
 };
 
 /**
  * Ограничение количества элементов в коллекции
  * @param {Number} count – Максимальное количество элементов
+ * @returns {function}
  */
 exports.limit = function (count) {
-    console.info(count);
+    return function limit(collection) {
+        let resultCollection = [];
+        for (let i = 0; i < count; i++) {
+            resultCollection.push(collection[i]);
+        }
 
-    return;
+        return resultCollection;
+    };
 };
 
 if (exports.isStar) {
@@ -73,17 +122,45 @@ if (exports.isStar) {
      * Фильтрация, объединяющая фильтрующие функции
      * @star
      * @params {...Function} – Фильтрующие функции
+     * @returns {function}
      */
     exports.or = function () {
-        return;
+        const functions = [].slice.call(arguments);
+
+        return function or(collection) {
+            return collection.filter(person => {
+                let isGoodPerson = false;
+                functions.forEach(func => {
+                    if (func([person]).length > 0) {
+                        isGoodPerson = true;
+                    }
+                });
+
+                return isGoodPerson;
+            });
+        };
     };
 
     /**
      * Фильтрация, пересекающая фильтрующие функции
      * @star
      * @params {...Function} – Фильтрующие функции
+     * @returns {function}
      */
     exports.and = function () {
-        return;
+        const functions = [].slice.call(arguments);
+
+        return function and(collection) {
+            return collection.filter(person => {
+                let isGoodPerson = true;
+                functions.forEach(func => {
+                    if (func([person]).length <= 0) {
+                        isGoodPerson = false;
+                    }
+                });
+
+                return isGoodPerson;
+            });
+        };
     };
 }
