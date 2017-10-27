7497fc683b5a95b5d39cfd2edf588387085b7861
Ludmilishe <luda_babich@mail.ru>
2017-10-28 00:08:21 +0500

    Первый полёт

--- lego.js
+++ lego.js
@@ -6,65 +6,106 @@
  */
 exports.isStar = true;
 
+let priority = ['filterIn', 'sortBy', 'and', 'or', 'format', 'select', 'limit'];
+
 /**
  * Запрос к коллекции
  * @param {Array} collection
  * @params {...Function} – Функции для запроса
  * @returns {Array}
  */
-exports.query = function (collection) {
-    return collection;
+exports.query = function (collection, ...functions) {
+    let copiedCollection = JSON.parse(JSON.stringify(collection));
+
+    let sortedFuncs = functions.sort(function (func1, func2) {
+        return priority.indexOf(func1.name) - priority.indexOf(func2.name);
+    });
+
+    for (let func in sortedFuncs) {
+        if (functions.hasOwnProperty(func)) {
+            copiedCollection = functions[func](copiedCollection);
+        }
+    }
+
+    return copiedCollection;
 };
 
 /**
  * Выбор полей
  * @params {...String}
+ * @returns {Function}
  */
-exports.select = function () {
-    return;
+exports.select = function (...attrs) {
+    return function select(collection) {
+        return collection.map(function (friend) {
+            let newObj = {};
+            attrs.forEach(function (attr) {
+                newObj[attr] = friend[attr];
+            });
+
+            return newObj;
+        });
+    };
 };
 
 /**
  * Фильтрация поля по массиву значений
  * @param {String} property – Свойство для фильтрации
  * @param {Array} values – Доступные значения
+ * @returns {Function}
  */
 exports.filterIn = function (property, values) {
-    console.info(property, values);
-
-    return;
+    return function filterIn(collection) {
+        return collection.filter (function (friend) {
+            return values.some(function (value) {
+                return value === friend[property];
+            });
+        });
+    };
 };
 
 /**
  * Сортировка коллекции по полю
  * @param {String} property – Свойство для фильтрации
  * @param {String} order – Порядок сортировки (asc - по возрастанию; desc – по убыванию)
+ * @returns {Function}
  */
 exports.sortBy = function (property, order) {
-    console.info(property, order);
+    return function sortBy(collection) {
+        let sorted = collection.sort(function (a, b) {
+            return a[property] > b[property];
+        });
 
-    return;
+        return order === 'asc' ? sorted : sorted.reverse();
+    };
 };
 
 /**
  * Форматирование поля
  * @param {String} property – Свойство для фильтрации
  * @param {Function} formatter – Функция для форматирования
+ * @returns {Function}
  */
 exports.format = function (property, formatter) {
-    console.info(property, formatter);
+    return function format(collection) {
+        return collection.map(function (friend) {
+            friend[property] = formatter(friend[property]);
 
-    return;
+            return friend;
+        });
+    };
 };
 
 /**
  * Ограничение количества элементов в коллекции
  * @param {Number} count – Максимальное количество элементов
+ * @returns {Function}
+ * @returns {Function}
  */
 exports.limit = function (count) {
-    console.info(count);
-
-    return;
+    return function limit(collection) {
+        return collection.slice(0, count);
+    };
 };
 
 if (exports.isStar) {
@@ -73,17 +114,33 @@ if (exports.isStar) {
      * Фильтрация, объединяющая фильтрующие функции
      * @star
      * @params {...Function} – Фильтрующие функции
+     * @returns {Function}
      */
-    exports.or = function () {
-        return;
+    exports.or = function (...funcs) {
+        return function or(collection) {
+            let initialCollection = JSON.parse(JSON.stringify(collection));
+            let col = [];
+            funcs.forEach(function (func) {
+                col = col.concat(func(initialCollection));
+            });
+
+            return col;
+        };
     };
 
     /**
      * Фильтрация, пересекающая фильтрующие функции
      * @star
      * @params {...Function} – Фильтрующие функции
+     * @returns {Function}
      */
-    exports.and = function () {
-        return;
+    exports.and = function (...funcs) {
+        return function and(collection) {
+            funcs.forEach(function (func) {
+                collection = func(collection);
+            });
+
+            return collection;
+        };
     };
 }
