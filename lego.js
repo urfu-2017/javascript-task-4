300215e156b9d16588de3aab57138d4c547e7b70
Ludmilishe <luda_babich@mail.ru>
2017-10-28 06:15:52 +0500

    Откат

--- lego.js
+++ lego.js
@@ -66,20 +66,16 @@ exports.select = function (...attrs) {
             return [];
         }
 
-        return collection
-            .filter(function (friend) {
-                return attrs.every((attr) => friend[attr] !== undefined);
-            })
-            .map(function (friend) {
-                let newObj = {};
-                attrs.forEach(function (attr) {
-                    if (friend.hasOwnProperty(attr)) {
-                        newObj[attr] = friend[attr];
-                    }
-                });
-
-                return newObj;
+        return collection.map(function (friend) {
+            let newObj = {};
+            attrs.forEach(function (attr) {
+                if (friend.hasOwnProperty(attr)) {
+                    newObj[attr] = friend[attr];
+                }
             });
+
+            return newObj;
+        });
     }
 
     return { func: select, args: attrs };
