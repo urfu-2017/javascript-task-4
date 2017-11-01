7e35c447ea1699213eb8ede97f22941dfea7a0e0
Ludmilishe <luda_babich@mail.ru>
2017-11-01 06:08:24 +0500

    Тесты

--- lego.js
+++ lego.js
@@ -76,7 +76,7 @@ exports.filterIn = function (property, values) {
 exports.sortBy = function (property, order) {
     return function sortBy(collection) {
         return collection.sort(function (a, b) {
-            return order === 'asc' ? a[property] - b[property] : b[property] - a[property];
+            return order === 'asc' ? a[property] < b[property] : b[property] < a[property];
         });
     };
 };
