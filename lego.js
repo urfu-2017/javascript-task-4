73cef48c1dd5d71f3841395d2bfbee630a00d5f3
Solodovnikova Ekaterina <colo18@yandex.ru>
2017-11-02 18:08:06 +0500

    some style fixes

--- lego.js
+++ lego.js
@@ -22,8 +22,7 @@ const funcPriority = {
  * @param {Array} collection
  * @returns {Array}
  */
-exports.query = function (collection) {
-    let functions = [].slice.call(arguments, 1);
+exports.query = function (collection, ...functions) {
     let copyArray = collection.map(friend => Object.assign({}, friend));
     functions.sort((a, b) => funcPriority[a.name] > funcPriority[b.name]);
     for (let func of functions) {
@@ -63,8 +62,8 @@ exports.filterIn = function (property, values) {
     return function filterIn(collection) {
         let changedCollection = [];
         for (let item of collection) {
-            let isFound = values.some(x => {
-                return item[property] === x;
+            let isFound = values.some((findingItems) => {
+                return item[property] === findingItems;
             });
             if (isFound) {
                 changedCollection.push(item);
@@ -85,14 +84,12 @@ exports.sortBy = function (property, order) {
     return function sortBy(collection) {
         return collection.sort(
             function (a, b) {
-                if (a[property] < b[property]) {
-                    return (order === 'asc') ? -1 : 1;
-                }
-                if (a[property] > b[property]) {
-                    return (order === 'asc') ? 1 : -1;
+                var countedValue = a[property] - b[property];
+                if (countedValue === 0) {
+                    return 0;
                 }
 
-                return 0;
+                return (order === 'asc') ? Math.sign(countedValue) : (-1) * Math.sign(countedValue);
             }
         );
     };
